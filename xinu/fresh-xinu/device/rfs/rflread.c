3091e49e341c9a24d914019a5d3e103602dd77d6
andersondav <andersondavid2000@gmail.com>
2021-03-01 15:01:57 -0500

    continued caching progress

--- xinu/fresh-xinu/device/rfs/rflread.c
+++ xinu/fresh-xinu/device/rfs/rflread.c
@@ -6,121 +6,6 @@
  * rflread  -  Read data from a remote file
  *------------------------------------------------------------------------
  */
-devcall	rflread (
-	  struct dentry	*devptr,	/* Entry in device switch table	*/
-	  char	*buff,			/* Buffer of bytes		*/
-	  int32	count 			/* Count of bytes to read	*/
-	)
-{
-	struct	rflcblk	*rfptr;		/* Pointer to control block	*/
-	int32	retval;			/* Return value			*/
-	struct	rf_msg_rreq  msg;	/* Request message to send	*/
-	struct	rf_msg_rres resp;	/* Buffer for response		*/
-	int32	i;			/* Counts bytes copied		*/
-	char	*from, *to;		/* Used during name copy	*/
-	int32	len;			/* Length of name		*/
-
-	/* Wait for exclusive access */
-
-	wait(Rf_data.rf_mutex);
-
-	/* Verify count is legitimate */
-
-	if ( (count <= 0) || (count > RF_DATALEN) ) {
-		signal(Rf_data.rf_mutex);
-		return SYSERR;
-	}
-
-	/* Verify pseudo-device is in use */
-
-	rfptr = &rfltab[devptr->dvminor];
-
-	/* If device not currently in use, report an error */
-
-	if (rfptr->rfstate == RF_FREE) {
-		signal(Rf_data.rf_mutex);
-		return SYSERR;
-	}
-
-	/* Verify pseudo-device allows reading */
-
-	if ((rfptr->rfmode & RF_MODE_R) == 0) {
-		signal(Rf_data.rf_mutex);
-		return SYSERR;
-	}
-
-	/* Form read request */
-
-	msg.rf_type = htons(RF_MSG_RREQ);
-	msg.rf_status = htons(0);
-	msg.rf_seq = 0;			/* Rfscomm will set sequence	*/
-	from = rfptr->rfname;
-	to = msg.rf_name;
-	memset(to, NULLCH, RF_NAMLEN);	/* Start name as all zero bytes	*/
-	len = 0;
-	while ( (*to++ = *from++) ) {	/* Copy name to request		*/
-		if (++len >= RF_NAMLEN) {
-			signal(Rf_data.rf_mutex);
-			return SYSERR;
-		}
-	}
-	msg.rf_pos = htonl(rfptr->rfpos);/* Set file position		*/
-	msg.rf_len = htonl(count);	/* Set count of bytes to read	*/
-
-	/* Send message and receive response */
-
-	retval = rfscomm((struct rf_msg_hdr *)&msg,
-					sizeof(struct rf_msg_rreq),
-			 (struct rf_msg_hdr *)&resp,
-					sizeof(struct rf_msg_rres) );
-
-	/* Check response */
-
-	if (retval == SYSERR) {
-		signal(Rf_data.rf_mutex);
-		return SYSERR;
-	} else if (retval == TIMEOUT) {
-		kprintf("Timeout during remote file read\n");
-		signal(Rf_data.rf_mutex);
-		return SYSERR;
-	} else if (ntohs(resp.rf_status) != 0) {
-		signal(Rf_data.rf_mutex);
-		return SYSERR;
-	}
-
-	/* Copy data to application buffer and update file position */
-	kprintf("The returned file size was %u bytes!\n", ntohl(resp.rf_size));
-	rfptr->rfsize = ntohl(resp.rf_size);
-
-	/* Determine number of bytes to copy into buffer */
-	uint32 copy_length;
-	if (ntohl(resp.rf_len) > count) {
-		copy_length = count;
-	}
-	else {
-		copy_length = ntohl(resp.rf_len);
-	}
-
-	/* 
-	 * Instead of always copying reply length,
-	 * copy as much data without overflowing the buffer.
-	 */
-	// for (i=0; i<ntohl(resp.rf_len); i++) {
-	// 	*buff++ = resp.rf_data[i];
-	// }
-	// rfptr->rfpos += ntohl(resp.rf_len);
-	for (i=0; i<copy_length; i++) {
-		*buff++ = resp.rf_data[i];
-	}
-	rfptr->rfpos += copy_length;
-	
-
-	signal(Rf_data.rf_mutex);
-	//return ntohl(resp.rf_len);
-	return copy_length;
-}
-
-/* updated implementation that utilizes caching */
 // devcall	rflread (
 // 	  struct dentry	*devptr,	/* Entry in device switch table	*/
 // 	  char	*buff,			/* Buffer of bytes		*/
@@ -134,7 +19,6 @@ devcall	rflread (
 // 	int32	i;			/* Counts bytes copied		*/
 // 	char	*from, *to;		/* Used during name copy	*/
 // 	int32	len;			/* Length of name		*/
-// 	uint32  cached_bytes;			/* Is desired data in cache */
 
 // 	/* Wait for exclusive access */
 
@@ -163,14 +47,6 @@ devcall	rflread (
 // 	if ((rfptr->rfmode & RF_MODE_R) == 0) {
 // 		signal(Rf_data.rf_mutex);
 // 		return SYSERR;
-// 	}
-
-// 	/* check if desired data exists in cache; place in buff if so */
-// 	cached_bytes = rfs_cache_fetch(rfptr, buff, count);
-
-// 	/* if data was cached, return; otherwise, send request */
-// 	if (cached_bytes == count) {
-// 		return count;
 // 	}
 
 // 	/* Form read request */
@@ -213,7 +89,8 @@ devcall	rflread (
 // 	}
 
 // 	/* Copy data to application buffer and update file position */
-// 	// kprintf("Your read request returned %u bytes!\n", ntohl(resp.rf_len));
+// 	kprintf("The returned file size was %u bytes!\n", ntohl(resp.rf_size));
+// 	rfptr->rfsize = ntohl(resp.rf_size);
 
 // 	/* Determine number of bytes to copy into buffer */
 // 	uint32 copy_length;
@@ -242,3 +119,191 @@ devcall	rflread (
 // 	//return ntohl(resp.rf_len);
 // 	return copy_length;
 // }
+
+/* updated implementation that utilizes caching */
+devcall	rflread (
+	  struct dentry	*devptr,	/* Entry in device switch table	*/
+	  char	*buff,			/* Buffer of bytes		*/
+	  int32	count 			/* Count of bytes to read	*/
+	)
+{
+	struct	rflcblk	*rfptr;		/* Pointer to control block	*/
+	int32	retval;			/* Return value			*/
+	struct	rf_msg_rreq  msg;	/* Request message to send	*/
+	struct	rf_msg_rres resp;	/* Buffer for response		*/
+	int32	i;			/* Counts bytes copied		*/
+	char	*from, *to;		/* Used during name copy	*/
+	int32	len;			/* Length of name		*/
+
+	/* for caching */
+	uint32  bytes_retrieved;		/* Bytes retrieved */
+	uint32  positions[3];			/* positions of (potentially multiple) requests */
+	uint32  counts[3];				/* counts of (potentially multiple) requests */
+	uint32  i;						/* used for looping */
+
+	/* Wait for exclusive access */
+
+	wait(Rf_data.rf_mutex);
+
+	/* Verify count is legitimate */
+
+	if ( (count <= 0) || (count > RF_DATALEN) ) {
+		signal(Rf_data.rf_mutex);
+		return SYSERR;
+	}
+
+	/* Verify pseudo-device is in use */
+
+	rfptr = &rfltab[devptr->dvminor];
+
+	/* If device not currently in use, report an error */
+
+	if (rfptr->rfstate == RF_FREE) {
+		signal(Rf_data.rf_mutex);
+		return SYSERR;
+	}
+
+	/* Verify pseudo-device allows reading */
+
+	if ((rfptr->rfmode & RF_MODE_R) == 0) {
+		signal(Rf_data.rf_mutex);
+		return SYSERR;
+	}
+
+	/* truncate request so it does not go beyond current file size */
+	if (rfptr->rfpos + count > rfptr->rfsize) {
+		/* reduce count to be rfsize - rfpos bytes */
+		count = rfptr->rfsize - rfptr->rfpos;
+	}
+
+	/* re-check that count is valid */
+	if ( (count <= 0) || (count > RF_DATALEN) ) {
+		signal(Rf_data.rf_mutex);
+		return SYSERR;
+	}
+
+	/* if request requires 2 cache blocks, split into two requests */
+	if ((rfptr->rfpos + count) / RF_DATALEN > rfptr->rfpos / RF_DATALEN) {
+		/* request 1 will be from [rfpos, ((rfpos / RFDATALEN)+1)*RFDATALEN-1]) */
+		positions[1] = rfptr->rfpos;
+		// counts[1] = (rfptr->rfpos / RF_DATALEN)*RF_DATALEN + RF_DATALEN-1 - rfptr->rfpos;
+		counts[1] = (rfptr->rfpos & (~RF_DATALEN)) + RF_DATALEN-1 - rfptr->rfpos;
+
+		/* request 2 will be for [(rfptr->rfpos / RF_DATALEN)*RF_DATALEN + RF_DATALEN, (rfptr->rfpos / RF_DATALEN)*RF_DATALEN + RF_DATALEN + count - counts[0]] */
+		// positions[2] = (rfptr->rfpos / RF_DATALEN)*RF_DATALEN + RF_DATALEN;
+		positions[2] = (rfptr->rfpos & (~RF_DATALEN)) + RF_DATALEN;
+		counts[2] = count - counts[0];
+	}
+	else {
+		/* request 1 (of 1) will be from [rfpos, rfpos + count] */
+		positions[1] = rfptr->rfpos;
+		counts[1] = count;
+
+		positions[2] = NULL;
+		counts[2] = NULL;
+	}
+
+	positions[0] = 0;
+	counts[0] = 0;
+	bytes_retrieved = 0;
+	/* loop through all valid requests */
+	for (i = 1; i < 3; i++) {
+		/* break once we reach an empty request */
+		if (positions[i] == NULL || counts[i] == NULL) {
+			break;
+		}
+
+		/* check if desired data exists in cache; place in buff if so */
+		bytes_retrieved = rfs_cache_fetch(rfptr, positions[i], buff + counts[i-1], counts[i]);
+
+		if (bytes_retrieved == counts[i]) {
+			/* got desired data */
+			continue;
+		}
+
+		/* Data not found in cache, form read request */
+
+		msg.rf_type = htons(RF_MSG_RREQ);
+		msg.rf_status = htons(0);
+		msg.rf_seq = 0;			/* Rfscomm will set sequence	*/
+		from = rfptr->rfname;
+		to = msg.rf_name;
+		memset(to, NULLCH, RF_NAMLEN);	/* Start name as all zero bytes	*/
+		len = 0;
+		while ( (*to++ = *from++) ) {	/* Copy name to request		*/
+			if (++len >= RF_NAMLEN) {
+				signal(Rf_data.rf_mutex);
+				return SYSERR;
+			}
+		}
+
+		/* request will be for a full cache block */
+		msg.rf_pos = htonl((positions[i] / RF_DATALEN) * RF_DATALEN); /* Set file position		*/
+		msg.rf_len = htonl(RF_DATALEN);	/* Set count of bytes to read	*/
+
+		/* Send message and receive response */
+
+		retval = rfscomm((struct rf_msg_hdr *)&msg,
+						sizeof(struct rf_msg_rreq),
+				(struct rf_msg_hdr *)&resp,
+						sizeof(struct rf_msg_rres) );
+
+		/* Check response */
+
+		if (retval == SYSERR) {
+			signal(Rf_data.rf_mutex);
+			return SYSERR;
+		} else if (retval == TIMEOUT) {
+			kprintf("Timeout during remote file read\n");
+			signal(Rf_data.rf_mutex);
+			return SYSERR;
+		} else if (ntohs(resp.rf_status) != 0) {
+			signal(Rf_data.rf_mutex);
+			return SYSERR;
+		}
+
+		/* Update file size */
+		kprintf("The returned file size was %u bytes!\n", ntohl(resp.rf_size));
+		rfptr->rfsize = ntohl(resp.rf_size);
+
+		/* TODO: Cache data received from request and re-fetch from cache */
+		
+		/* Code for copying data to cache goes here */
+		// /* Copy data to application buffer and update file position */
+	
+		// /* Determine number of bytes to copy into buffer */
+		// uint32 copy_length;
+		// if (ntohl(resp.rf_len) > count) {
+		// 	copy_length = count;
+		// }
+		// else {
+		// 	copy_length = ntohl(resp.rf_len);
+		// }
+
+		// /* 
+		// * Instead of always copying reply length,
+		// * copy as much data without overflowing the buffer.
+		// */
+		// // for (i=0; i<ntohl(resp.rf_len); i++) {
+		// // 	*buff++ = resp.rf_data[i];
+		// // }
+		// // rfptr->rfpos += ntohl(resp.rf_len);
+		// for (i=0; i<copy_length; i++) {
+		// 	*buff++ = resp.rf_data[i];
+		// }
+		// rfptr->rfpos += copy_length;
+
+		/* Re-fetch data from cache */
+		bytes_retrieved = rfs_cache_fetch(rfptr, positions[i], buff + counts[i-1], counts[i]);
+
+		if (bytes_retrieved != counts[i]) {
+			/* can't reach data even after making request to data, there is a problem */
+			signal(Rf_data.rf_mutex);
+			return SYSERR;
+		}
+	}	
+
+	signal(Rf_data.rf_mutex);
+	//return ntohl(resp.rf_len);
+	return count;
+}
--- xinu/fresh-xinu/device/rfs/rfscache.c
+++ xinu/fresh-xinu/device/rfs/rfscache.c
@@ -1,86 +1,70 @@
-// /* rfscache - Collection of functions needed for the remote file system cache */
-// #include <xinu.h>
+/* rfscache - Collection of functions needed for the remote file system cache */
+#include <xinu.h>
 
-// /* byte_to_index - convertes a desired byte number to the correct index to look for in the cache */
-// void byte_to_index(
-//     uint32 byte,
-//     struct cache_pos *result) 
-// {
-//     if (byte < 0) {
-//         return SYSERR;
-//     }
-//     result->index = byte / RFL_BLOCK_SIZE;
-//     result->offset = byte % RFL_BLOCK_SIZE;
-//     return OK;
-// }
+/* byte_to_index - convertes a desired byte number to the correct index to look for in the cache */
+void byte_to_index(
+    uint32 byte,
+    struct cache_pos *result) 
+{
+    if (byte < 0) {
+        return SYSERR;
+    }
+    // result->index = byte / RFL_BLOCK_SIZE;
+    // result->offset = byte % RFL_BLOCK_SIZE;
+    result->index = byte >> 10;
+    result->offset = byte & RF_DATALEN - 1;
+    return OK;
+}
 
-// /* rfs_cache_fetch - Copies over as many bytes as possible from cache into buffer */
-// uint32 rfs_cache_fetch(
-//     struct rflcblk *rfptr,     /* pointer to remote file's control block */
-//     char *buff,                 /* user's buffer */
-//     int32 count)                /* count of desired bytes read */ 
-// {
-//     struct cache_pos cache_loc; /* location of target bytes in cache */
-//     uint32 status;              /* used to verify status of various helper calls */
-//     uint32 bytes_found;         /* number of bytes of file data found */
-//     struct rfl_block *cur_block;/* current block we are examining */
-//     char *from, *to;            /* Used for copying from cache to user buffer */
+/* rfs_cache_fetch - Copies over as many bytes as possible from cache into buffer */
+uint32 rfs_cache_fetch(
+    struct rflcblk *rfptr,      /* pointer to remote file's control block */
+    uint32 position,            /* position of requested data */
+    char *buff,                 /* user's buffer */
+    int32 count)                /* count of desired bytes read */ 
+{
+    struct cache_pos cache_loc; /* location of target bytes in cache */
+    uint32 status;              /* used to verify status of various helper calls */
+    uint32 bytes_found;         /* number of bytes of file data found */
+    struct rfl_block *cur_block;/* current block we are examining */
+    char *from, *to;            /* Used for copying from cache to user buffer */
 
-//     /* find correct location in cache */
-//     status = byte_to_index(rfptr->rfpos, &cache_loc);
-//     if (status == SYSERR) {
-//         /* invalid byte position */
-//         return 0;
-//     }
+    /* check that position is legal */
+    if (position < 0 || position > rfptr->rfsize) {
+        return SYSERR;
+    }
 
-//     /* check if desired bytes lie in multiple cache blocks */
-//     if (cache_loc->offset + count > RFL_BLOCK_SIZE) {
-//         /* user needs bytes from multiple cache blocks */
+    /* find correct location in cache */
+    byte_to_index(rfptr->rfpos, &cache_loc);
 
-//         /* fetch all bytes from first block */
-//         bytes_found = rfs_cache_fetch(rfptr, buff, RFL_BLOCK_SIZE - cache_loc->offset);
-//         if (bytes_found == RFL_BLOCK_SIZE - cache_loc->offset) {
-//             /* found all bytes from first block, can proceed to look for remaining bytes to fill rest of buff */
-//             bytes_found += rfs_cache_fetch(rfptr, buff + bytes_found, count - bytes_found); /* should really try to avoid this recursion */
-//             return bytes_found;
-//         }
-//         else {
-//             /* fetch from first block failed for some reason, either because ran into EOF or that block isn't in cache */
-//             /* (should try to truncate read to file length so we know that we get here b/c block isn't in cache) */
+    /* if we reach here, we know all requested bytes lie in same cache block */
+    if (cache_loc->index < MAX_BLOCKS) {
+        /* block resides in array structure, directly index in */
+        if (rfptr->rfl_cache[cache_loc.index] == NULL || rfptr->rfl_cache[cache_loc.index]->valid_bytes == 0) {
+            /* desired block not in cache */
+            /* we'll make a network request to bring this block into cache, then try fetching again, so this fetch fails */
+            return 0;
+        }
+        else {
+            /* retrieve pointer to cache block */
+            cur_block = rfptr->rfl_cache[cache_loc.index];
 
-//             /* we'll fetch all required blocks, then try fetching again, so this fetch fails */
-//             return 0;
-//         }
-//     }
+            /* copy until no longer in valid byte range or exceed count */
+            bytes_found = 0;
+            to = buff;
+            from = cur_block->block_data[cache_loc.offset];
+            while (cache_loc.offset < cur_block->valid_bytes && bytes_found < count) {
+                *to++ = *from++;
+                cache_loc.offset++;
+                bytes_found++;
+            }
 
-//     /* if we reach here, we know all requested bytes lie in same cache block */
-//     if (cache_loc->index < MAX_BLOCKS) {
-//         /* block resides in array structure, directly index in */
-//         if (rfptr->rfl_cache[cache_loc.index] == NULL) {
-//             /* desired block not in cache */
-//             /* we'll fetch the block later, then try fetching again, so this fetch fails */
-//             return 0;
-//         }
-//         else {
-//             /* retrieve pointer to cache block */
-//             cur_block = rfptr->rfl_cache[cache_loc.index];
+            return bytes_found;
+        }
+    }
+    else {
+        /* have to look in linked list, search for desired block */
+    }
 
-//             /* copy until no longer in valid byte range */
-//             bytes_found = 0;
-//             to = buff;
-//             from = cur_block->block_data[cache_loc.offset];
-//             while (cache_loc.offset < cur_block->valid_bytes) {
-//                 *to++ = *from++;
-//                 cache_loc.offset++;
-//                 bytes_found++;
-//             }
-
-//             return bytes_found;
-//         }
-//     }
-//     else {
-//         /* have to look in linked list, search for desired block */
-//     }
-
-//     return bytes_found;
-// }
+    return bytes_found;
+}
